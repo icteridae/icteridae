052ac669615bd292baf92b25139fa746a28b51d2
Dennis Hoebelt <de-hoebelt@t-online.de>
2020-12-15 22:01:20 +0100

    update Navbar

--- src/Common/Navbar.jsx
+++ src/Common/Navbar.jsx
@@ -28,25 +28,23 @@ class NavBarInstance extends React.Component {
         return (
             <Navbar {...this.state.props}>
                 <Navbar.Header>
-                <a href="#" className="navbar-brand logo">
-                    Icteridae
-                </a>
+                    <div className="navbar-brand">Icteridae</div>
             </Navbar.Header>
         <Navbar.Body>
             <Nav onSelect={this.handleSelect} activeKey={this.state.activeKey}>
-                <Nav.Item eventKey="1" icon={<Icon icon="home" />}>
-                    Home
+                <Nav.Item eventKey="1" icon={<Icon icon="search" />}>
+                    Search
                 </Nav.Item>
-                <Nav.Item eventKey="2">News</Nav.Item>
-                <Nav.Item eventKey="3">Products</Nav.Item>
-                <Dropdown title="About" toggleComponentClass={Button} appearance="default">
-                    <Dropdown.Item eventKey="4" icon={<Icon icon={"steam"} />} > <a href={"https://github.com/icteridae"} target="_blank">Github</a> </Dropdown.Item>
-                    <Dropdown.Item eventKey="5">Team</Dropdown.Item>
-                    <Dropdown.Item eventKey="6">Contact</Dropdown.Item>
+                <Nav.Item eventKey="2" icon={<Icon icon="bookmark" /> }>My Papers</Nav.Item>
+                <Nav.Item eventKey="3" icon={<Icon icon="circle-thin" /> }>Graph</Nav.Item>
+                <Dropdown title="About" icon={<Icon icon="info" />} toggleComponentClass={Button} appearance="default">
+                    <Dropdown.Item eventKey="4" icon={<Icon icon="file-text" />} >Description</Dropdown.Item>
+                    <Dropdown.Item eventKey="5" icon={<Icon icon="group" />} >Contact</Dropdown.Item>
                 </Dropdown>
             </Nav>
             <Nav pullRight>
-                <Nav.Item icon={<Icon icon="cog" />}>Settings</Nav.Item>
+                <Nav.Item eventKey="6" icon={<Icon icon={"github"} />} href={"https://github.com/icteridae/icteridae"} target="_blank" > Github </Nav.Item>
+                <Nav.Item eventkey="7" icon={<Icon icon="cog" />}>Settings</Nav.Item>
             </Nav>
         </Navbar.Body>
             </Navbar>
