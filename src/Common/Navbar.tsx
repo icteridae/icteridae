76248fb476ba3582107b1773b3892bedf6474ce5
lennart.mischnaewski <lennart.mischnaewski@stud.tu-darmstadt.de>
2020-12-18 12:26:18 +0100

    Refactor Navbar for typescript

--- src/Common/Navbar.tsx
+++ src/Common/Navbar.tsx
@@ -1,55 +1,32 @@
-
 import * as React from "react";
-import { Navbar, Nav, Icon, Dropdown, Button, NavbarProps } from 'rsuite';
-
+import { Navbar, Nav, Icon, Dropdown, Button, NavbarProps, IconProps } from 'rsuite';
 import './styles/Navbar.css'
 
-interface State {
-    activeKey: String | null
+export const NavBarInstance: React.FC<NavbarProps> = (props) => {
+    const [activeKey, handleSelect] = React.useState<string | null>(null);
+
+    return (
+        <Navbar {...props}>
+            <Navbar.Header>
+                <div className="navbar-brand">Icteridae</div>
+            </Navbar.Header>
+            <Navbar.Body>
+                <Nav onSelect={handleSelect} activeKey={activeKey}>
+                    <Nav.Item eventKey="search" icon={<Icon icon="search" />}>
+                        Search
+                    </Nav.Item>
+                    <Nav.Item eventKey="mypapers" icon={<Icon icon="bookmark" /> }>My Papers</Nav.Item>
+                    <Nav.Item eventKey="graph" icon={<Icon icon="circle-thin" /> }>Graph</Nav.Item>
+                    <Dropdown eventKey="dropdown" title="About" icon={<Icon icon="info" />} toggleComponentClass={Button} appearance="default">
+                        <Dropdown.Item eventKey="description" icon={<Icon icon="file-text" />} >Description</Dropdown.Item>
+                        <Dropdown.Item eventKey="contact" icon={<Icon icon="group" />} >Contact</Dropdown.Item>
+                    </Dropdown>
+                </Nav>
+                <Nav pullRight>
+                    <Nav.Item eventKey="github" icon={<Icon icon={"github"} />} href={"https://github.com/icteridae/icteridae"} target="_blank" > Github </Nav.Item>
+                    <Nav.Item eventkey="settings" icon={<Icon icon="cog" />}>Settings</Nav.Item>
+                </Nav>
+            </Navbar.Body>
+        </Navbar>
+    )
 }
-
-class NavBarInstance extends React.Component<NavbarProps, State> {
-    constructor(props: NavbarProps) {
-        super(props);
-        this.handleSelect = this.handleSelect.bind(this);
-        this.state = {
-            activeKey: null,
-        };
-    }
-
-    handleSelect(eventKey: String | null) {
-        this.setState({
-            activeKey: eventKey
-        });
-    }
-
-
-    render() {
-        return (
-            <Navbar {...this.props}>
-                <Navbar.Header>
-                    <div className="navbar-brand">Icteridae</div>
-                </Navbar.Header>
-                <Navbar.Body>
-                    <Nav onSelect={this.handleSelect} activeKey={this.state.activeKey}>
-                        <Nav.Item eventKey="1" icon={<Icon icon="search" />}>
-                            Search
-                        </Nav.Item>
-                        <Nav.Item eventKey="2" icon={<Icon icon="bookmark" /> }>My Papers</Nav.Item>
-                        <Nav.Item eventKey="3" icon={<Icon icon="circle-thin" /> }>Graph</Nav.Item>
-                        <Dropdown title="About" icon={<Icon icon="info" />} toggleComponentClass={Button} appearance="default">
-                            <Dropdown.Item eventKey="4" icon={<Icon icon="file-text" />} >Description</Dropdown.Item>
-                            <Dropdown.Item eventKey="5" icon={<Icon icon="group" />} >Contact</Dropdown.Item>
-                        </Dropdown>
-                    </Nav>
-                    <Nav pullRight>
-                        <Nav.Item eventKey="6" icon={<Icon icon={"github"} />} href={"https://github.com/icteridae/icteridae"} target="_blank" > Github </Nav.Item>
-                        <Nav.Item eventkey="7" icon={<Icon icon="cog" />}>Settings</Nav.Item>
-                    </Nav>
-                </Navbar.Body>
-            </Navbar>
-        )
-    }
-}
-
-export default NavBarInstance;
