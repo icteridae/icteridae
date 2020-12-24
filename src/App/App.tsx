fce339e4dc8d4dbcd48e46f49bbb91284d922823
lennart.mischnaewski <lennart.mischnaewski@stud.tu-darmstadt.de>
2020-12-24 20:51:16 +0100

    Refactor App component structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './../logo.svg';
 import './App.css';
-import {NavBarInstance} from './Navbar/Navbar';
+import {NavBarInstance} from '../Navbar/Navbar';
 
 function App() {
   return (
