a7d21081178ce9a5b83b51cde6faad11bc275e5a
lennart.mischnaewski <lennart.mischnaewski@stud.tu-darmstadt.de>
2020-12-24 21:21:38 +0100

    Refactor App component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,18 +3,13 @@ import {NavBarInstance} from '../Navbar/Navbar';
 import {Default} from '../Default/Default';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <HashRouter>
-        <NavBarInstance appearance="subtle"/>
-        <Switch>
-            <Route exact path='/' component={Default}/> 
-            {/* TODO: insert other routes */}
-        </Switch>
-      </HashRouter>
-    </div>
+export const App: React.FC = () => (
+    <HashRouter>
+      <NavBarInstance appearance="subtle"/>
+      <Switch>
+          <Route exact path='/' component={Default}/> 
+          {/* TODO: insert other routes. See paths in Navbar.tsx */}
+      </Switch>
+    </HashRouter>
   );
-}
 
-export default App;
