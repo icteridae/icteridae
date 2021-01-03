e5142f29dc1eb01429462f8df3deff399b224723
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-01-03 21:54:36 +0100

    import for stylesheet added, style property moved to stylesheet

--- src/pageSearchResult/PageSearchResult.tsx
+++ src/pageSearchResult/PageSearchResult.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import {PanelGroup, Panel, Placeholder, Grid, Row, Col} from 'rsuite';
+import SearchResultCard from './SearchResultCard';
+import './styles/PageSearchResult.css'
+
+const data = {
+    title: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+    authors: ["autor1", "autor2", "autor3"],
+    date: "1999",
+    citations: 200,
+    preview: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+}
 
 function PageSearchResult () {
-    return null;
+    return (
+        <div className="resultList">
+            <SearchResultCard data={data} />
+            <SearchResultCard data={data} />
+        </div>
+    );
 }
 
 export default PageSearchResult;
