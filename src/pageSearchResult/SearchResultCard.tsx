70c2125fd4697d3b20f10c05d8a8affa486b71bc
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-01-03 23:15:27 +0100

    classname card changed to content, number of chars in preview changed, tag for title changed

--- src/pageSearchResult/SearchResultCard.tsx
+++ src/pageSearchResult/SearchResultCard.tsx
@@ -16,12 +16,12 @@ type SearchResultCardProps = {
 const SearchResultCard : React.FC<SearchResultCardProps> = (props) => {
     return (
         <Panel className="searchResultCard">
-            <div className="card">
-                <h5 className="title">{props.data.title}</h5>
+            <div className="content">
+                <h3 className="title">{props.data.title}</h3>
                 <span className="author">{props.data.authors.join(", ")}</span>
                 <span className="date">{props.data.date}</span>
                 <span className="citations">{props.data.citations + " Citations"}</span>
-                <span className="previewText">{props.data.preview.substr(0, 200) + "..."}</span>
+                <span className="previewText">{props.data.preview.substr(0, 283) + "..."}</span>
             </div>
         </Panel>
     );
