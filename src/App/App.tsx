e453f6bf16b8bc7238e4aeec0a4626933ae28fee
Alexander Sinkovic <alexander.sinkovic@gmail.de>
2021-01-04 13:33:41 +0100

    Add Privacy.css und .tsx. Add Route to Privacy to Router. Add Nav.Item to Navbar

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {NavBarInstance} from '../Navbar/Navbar';
 import {Default} from '../Default/Default';
+import {Privacy} from '../Privacy/Privacy';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
 export const App: React.FC = () => (
     <HashRouter>
       <NavBarInstance appearance="subtle"/>
       <Switch>
-          <Route exact path='/' component={Default}/> 
+          <Route exact path='/' component={Default}/>
+          <Route exact path='/privacy' component={Privacy}/>
           {/* TODO: insert other routes. See paths in Navbar.tsx */}
       </Switch>
     </HashRouter>
   );
-
