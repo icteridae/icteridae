cf74858daa03aa2eb4f98b9de5b8299064a39ffd
Nico <nico.kunz@stud.tu-darmstadt.de>
2021-01-06 19:16:52 +0100

    Effect hook added to SearchResultList that dynamically changes the height of the list. ids added to Navbar and SearchResultList

--- src/Components/Search/SearchResult/SearchResultList.tsx
+++ src/Components/Search/SearchResult/SearchResultList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import './styles/SearchResultList.css';
 import SearchResultCard from "./SearchResultCard";
@@ -19,8 +19,26 @@ type ResultListProps = {
 }
 
 const SearchResultList : React.FC<ResultListProps> = (props) => {
+    // Effect hook for dynamically changing the height of the resultList and thus getting a scrollbar BECAUSE SCROLLBARS
+    useEffect(() => {
+        function setListToRemainingHeight() {
+            let windowHeight = window.innerHeight;
+            // @ts-ignore
+            let navbarHeight = document.getElementById("navbar").offsetHeight;
+
+            let list = document.getElementById("list");
+
+            // @ts-ignore
+            list.style.height = (windowHeight - navbarHeight) + "px";
+            console.log("test");
+        }
+
+        setListToRemainingHeight();
+        window.addEventListener('resize', setListToRemainingHeight);
+    }, []);
+
     return (
-        <div className="resultList">
+        <div id="list" className="resultList">
             <h2>Showing search results for <b>"{props.query}"</b>:</h2>
             {props.data.map((entry) => {
                 return <SearchResultCard func={props.func} key={entry.key} data={entry}/>
