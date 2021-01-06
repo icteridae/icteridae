f3fa8153ffba1235a8b47e503e00ea88681d1354
Alexander Sinkovic <alexander.sinkovic@gmail.de>
2021-01-06 18:34:23 +0100

    Revert "add Graph Tab"
    This reverts commit 1b421363

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {NavBarInstance} from '../Navbar/Navbar';
 import {Default} from '../Default/Default';
 import {Privacy} from '../Privacy/Privacy';
-import {Graph} from '../Graph/Graph';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
  export const App: React.FC = () => (
@@ -11,7 +10,6 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
       <Switch>
           <Route exact path='/' component={Default}/>
           <Route exact path='/privacy' component={Privacy}/>
-          <Route exact path='/graph' component={Graph}/>
           {/* TODO: insert other routes. See paths in Navbar.tsx */}
       </Switch>
     </HashRouter>
