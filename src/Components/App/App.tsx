903463c16100ade0f6818171b6aae3c84780c432
Alexander Sinkovic <alexander.sinkovic@gmail.de>
2021-01-08 15:39:16 +0100

    Added Graph Stuff

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavBarInstance} from '../Navbar/Navbar';
 import {Default} from '../Default/Default';
 import {Privacy} from '../Privacy/Privacy';
+import {Graph} from '../Graph/Graph';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
  export const App: React.FC = () => (
@@ -10,6 +11,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
       <Switch>
           <Route exact path='/' component={Default}/>
           <Route exact path='/privacy' component={Privacy}/>
+          <Route exact path='/Graph' component={Graph}/>
           {/* TODO: insert other routes. See paths in Navbar.tsx */}
       </Switch>
     </HashRouter>
