73c89b454fceb9139c8d16f69c7a16e94bcfa777
joynere <nicokunznk00@gmail.com>
2021-01-08 23:34:13 +0100

    Change PageSearchResult declaration

--- frontend/src/Components/Search/SearchResult/PageSearchResult.tsx
+++ frontend/src/Components/Search/SearchResult/PageSearchResult.tsx
@@ -14,7 +14,7 @@ const initialState = {
     paperAbstract: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
 }
 
-function PageSearchResult () {
+const PageSearchResult = () => {
     const [selected, setSelected] = useState(initialState);
 
     return (
