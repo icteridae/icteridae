7180f26b518d1a01f5d81283b2d3b36e38fc1886
Inluminous <lenmisch@gmail.com>
2021-01-10 00:03:08 +0100

    Edit paper model. combine authorID and author

--- backend/graphgenerator/models.py
+++ backend/graphgenerator/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Paper(models.Model):
+class Paper(models.Model): # Independant
     """
     Model for a single paper
     """
@@ -10,12 +10,12 @@ class Paper(models.Model):
     id = models.CharField(max_length=40, primary_key=True)
 
     title = models.CharField(max_length=200) # TODO check max title length
-    paperAbstract = models.TextField()
+    paperAbstract = models.TextField(blank=True)
     
     authors = models.ManyToManyField('Author')
-    inCitations = models.ManyToManyField('self')
+    inCitations = models.ManyToManyField('self', symmetrical=False)
     # outCitations probably not needed?
-    year = models.IntegerField()
+    year = models.IntegerField(null=True)
     s2Url = models.URLField()
     # sources # TODO
     # venue # TODO
@@ -30,29 +30,22 @@ class Paper(models.Model):
     # s2PdUrl # TODO
     # entities # TODO
 
-class Author(models.Model):
+class Author(models.Model): # Independant
     name = models.CharField(max_length=200) 
+    id = models.CharField(max_length=100, primary_key=True)
 
     def __str__(self):
-        return self.name
-class AuthorID(models.Model):
-    id = models.IntegerField(primary_key=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+        return self.name 
 
-    def __str__(self):
-        return self.author.name + ', id: ' + str(self.id)
- 
-
-class FieldOfStudy(models.Model):
+class FieldOfStudy(models.Model): # Independant
     field = models.CharField(max_length=100, primary_key=True)
 
     def __str__(self):
         return self.field
 
-
-class PdfUrl(models.Model):
+class PdfUrl(models.Model): # Dependant on Paper
     url = models.URLField()
     paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
 
     def __str__(self):
-        return url
+        return self.url
