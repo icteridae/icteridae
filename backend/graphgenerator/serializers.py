013e7c3b2c3f3a9f71baf8d9b56c0b15f379e860
Inluminous <lenmisch@gmail.com>
2021-01-10 00:52:16 +0100

    Add Paper serializer

---
+++ backend/graphgenerator/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Paper
+
+class PaperSerializer(serializers.ModelSerializer):
+    outCitations = serializers.SerializerMethodField('getOutCitations')
+
+    def getOutCitations(self, paper):
+        return [paper.id for paper in paper.paper_set.all()]
+
+    class Meta:
+        model = Paper
+        fields = '__all__'
