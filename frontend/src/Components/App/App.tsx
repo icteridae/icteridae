0ba1b78a45dbeeb6864d25c74da14965bf84c65f
joynere <nicokunznk00@gmail.com>
2021-01-10 17:21:17 +0100

    Change  HashRouter to BrowserRouter

--- frontend/src/Components/App/App.tsx
+++ frontend/src/Components/App/App.tsx
@@ -3,16 +3,18 @@ import {NavBarInstance} from '../Navbar/Navbar';
 import {FrontPage} from '../Front/FrontPage';
 import {Privacy} from '../Privacy/Privacy';
 import {Graph} from '../Graph/Graph';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {PageSearchResult} from '../Search/SearchResult/PageSearchResult'; 
 
  export const App: React.FC = () => (
-    <HashRouter>
+    <BrowserRouter>
       <NavBarInstance appearance="subtle"/>
       <Switch>
           <Route exact path='/' component={FrontPage}/>
+          <Route exact path='/results' component={PageSearchResult}/>
           <Route exact path='/privacy' component={Privacy}/>
           <Route exact path='/Graph' component={Graph}/>
           {/* TODO: insert other routes. See paths in Navbar.tsx */}
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
