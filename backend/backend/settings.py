2a0d538f95019c1fe4e1bc05001926e24afc5abe
Inluminous <lenmisch@gmail.com>
2021-01-11 00:22:09 +0100

    Fix django using default postgresql database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'postgres',
+        'NAME': 'icteridae',
 
         'USER': 'postgres',
 
