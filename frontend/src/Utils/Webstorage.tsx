0ac8403a0b3fbaa0eb1b0c7dbb9bdcd08abfcbd7
Dennis Hoebelt <de-hoebelt@t-online.de>
2021-01-11 18:34:46 +0100

    create Webstorage wrapper for FrontPage

--- frontend/src/Utils/Webstorage.tsx
+++ frontend/src/Utils/Webstorage.tsx
@@ -1,11 +1,13 @@
 export function getRecentPapers(){
-    let  storedPapers : Array<string>|undefined = JSON.parse(localStorage.getItem("papers") as string);
+    let  storedPapers : Array<string> = JSON.parse(localStorage.getItem("papers") as string);
     return storedPapers;
 }
 
-export function setRecentPapers(props: Array<string> | undefined){
-    if(props !== undefined)
+export function setRecentPapers(props: Array<string>){
+    if(typeof(props) !== "undefined") {
+        localStorage.removeItem("papers");
         localStorage.setItem("papers", JSON.stringify(props));
-        alert("Succesfull saved");
+        alert("saved successfully");
+    }
     return;
 }
