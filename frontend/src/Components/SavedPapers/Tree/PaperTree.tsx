b509f952fc1208434a43ea395bc34a4179fbf722
Hoebelt <dennis.hoebelt@stud.tu-darmstadt.de>
2021-01-19 17:26:49 +0100

    Saved Papers are replaced with the titles of the paper

--- frontend/src/Components/SavedPapers/Tree/PaperTree.tsx
+++ frontend/src/Components/SavedPapers/Tree/PaperTree.tsx
@@ -3,45 +3,56 @@ import {Tree, Icon} from 'rsuite';
 import {getSavedPapers} from "../../../Utils/Webstorage";
 import Config from '../../../Utils/Config'
 
-const data = [
-    {
-        "label": <div><Icon icon='folder'/> Folder1</div>,
-        "value": "d1",
-    },
-    {
-        "label": "f977b5310ba86074d2be0fb86553b418a24a9273",
-        "value": "p2",
-    },
-    {
-        "label": <div><Icon icon='folder'/> Folder3</div>,
-        "value": "d3",
-        "children": [{
-            "label": "f977b5310ba86074d2be0fb86553b418a24a9273",
-            "value": "p4",
-        },{
-            "label": "3dd133251dffdc9480bed899bd62050f086bf43e",
-            "value": "p5",
+type TreeType = {label?: string, value: string, children?: TreeInterface[], id? :string};
+interface TreeInterface extends TreeType {};
+
+const data : TreeInterface[]= [
+        {
+            "label": /*<div><Icon icon='folder'/> Folder1</div> */ "Folder 1",
+            "value": "d1",
+        },
+        {
+            "id": "f977b5310ba86074d2be0fb86553b418a24a9273",
+            "value": "p2",
+        },
+        {
+            "label": /*<div><Icon icon='folder'/> Folder3</div>*/ "Folder 3",
+            "value": "d3",
+            "children": [{
+                "id": "f977b5310ba86074d2be0fb86553b418a24a9273",
+                "value": "p4",
+            }, {
+                "id": "3dd133251dffdc9480bed899bd62050f086bf43e",
+                "value": "p5",
+            }
+            ]
         }
-        ]
-    }
 ]
 
-const CreatePaperTreeData = (tree : any) => {
-    const baseURL : string = Config.base_url;
-    if(tree.value.charAt(0) === 'p') {
-        const id = tree.label;
-        fetch(baseURL+"/api/paper/?paper_id=" + id)
-            .then(res => res.json())
-            .then(res => tree.label = res.title)
-    }
-    else if (tree.value.charAt(0) === 'd' && tree.children) {
-         tree.children = CreatePaperTreeData(tree.children);
+const CreatePaperTreeData = (tree : Array<TreeInterface>) => {
+    console.log(tree);
+    for(let item in tree) {
+        //console.log(tree[item]);
+        if(tree[item].value.charAt(0) === 'p')
+        {
+            const baseURL : string = Config.base_url;
+            fetch(baseURL +"/api/paper/?paper_id=" + tree[item].id)
+                .then(res => res.json())
+                .then(res => {
+                    tree[item].label = res.title;
+                })
+        } else if (tree[item].value.charAt(0) === "d") {
+            if (tree[item].children) {
+                //@ts-ignore
+                tree[item].children = CreatePaperTreeData(tree[item].children);
+            }
+        }
     }
     return tree;
 }
 
 const PaperTree: React.FC<{choosePaper: Function, height: number}> = (props ) => {
-    const [treeData, setTreeData] = React.useState(data);
+    const [treeData, setTreeData] = React.useState(CreatePaperTreeData(data));
     return (
         <Tree
             data={treeData}
