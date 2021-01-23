c73f7018969ff407d6594ce5b982b241fcca7103
Inluminous <lenmisch@gmail.com>
2021-01-23 23:08:51 +0100

    Include rank_feature citations in Paper document for elasticsearch

--- backend/graphgenerator/documents.py
+++ backend/graphgenerator/documents.py
@@ -1,10 +1,21 @@
 from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl import fields
 from django_elasticsearch_dsl.registries import registry
+from elasticsearch_dsl import field
 from .models import Paper
 
+class FuckThis(fields.DEDField, field.RankFeature):
+    """Implements a RankFeature for django-elasticsearch-dsl as the current version does not support the given Class
+    """
+    pass
 
 @registry.register_document
 class PaperDocument(Document):
+    """Class for indexing Paper Elements in elasticsearch
+    """
+
+    citations = FuckThis(attr = 'get_citations') # RankFeature for citation field
+
     class Index:
 
         name = 'papers'
@@ -15,6 +26,7 @@ class PaperDocument(Document):
         }
 
     class Django:
+
         model = Paper
 
         fields = [
