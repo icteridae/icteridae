8b7ee2ba400d92b7ac912b7be30f2d47609e0851
Alexander Sinkovic <alexander.sinkovic@gmail.com>
2021-02-01 16:41:06 +0100

    transfer changes from local feature to this

--- frontend/src/Components/Graph/Graph.tsx
+++ frontend/src/Components/Graph/Graph.tsx
@@ -1,14 +1,17 @@
-import * as React from 'react';
-import ForceGraph2D, {GraphData, LinkObject, NodeObject} from 'react-force-graph-2d';
-import { forceRadial, forceLink } from "d3-force-3d";
-import {Button, Drawer, Row, Col, Slider, InputNumber} from "rsuite";
+import React from 'react';
+
 import Config from '../../Utils/Config';
+
+import ForceGraph2D, {GraphData, LinkObject, NodeObject} from 'react-force-graph-2d';
+import { forceRadial, forceLink } from 'd3-force-3d';
+import {Button, Drawer, Row, Col, Slider, InputNumber} from 'rsuite';
+
 import './Graph.css'
 
 /**
  * This interface characterizes a single Paper
  */
-interface paper extends NodeObject{
+interface Paper extends NodeObject{
     /** The unique ID of the paper */
     id : string,
 
@@ -82,32 +85,32 @@ interface paper extends NodeObject{
 /**
  * This interface describes a similarity
  */
-interface similarity{
+interface Similarity{
     /** The Name of the Similarity */
     name: string,
 
     /** A Description for the Similarity */
-    description: string
+    description: string,
 }
 
 /**
  * Json structure of the Response from /api/generate_graph/
  */
-interface papersAndSimilarities{
+interface PapersAndSimilarities{
     /** Tensor of all similarities for every pair of papers */
     tensor: number[][][],
 
     /** List of all papers that are relevant for the graph including the requested paper*/
-    paper: paper[],
+    paper: Paper[],
 
     /** List of all similarities used in this tensor */
-    similarities: similarity[]
+    similarities: Similarity[]
 }
 
 /**
  * This interface adds the similarity attribute to LinkObjects. Is only used if we include our own Link Force
  */
-interface myLinkObject extends LinkObject{
+interface MyLinkObject extends LinkObject{
     similarity: number,
     label: string,
 }
@@ -115,24 +118,24 @@ interface myLinkObject extends LinkObject{
 /**
  * This interface adjust the nodes and links of the graph to contain enough information
  */
-interface myGraphData extends GraphData{
-    nodes: paper[],
-    links: myLinkObject[],
+interface MyGraphData extends GraphData{
+    nodes: Paper[],
+    links: MyLinkObject[],
 }
 
 // Variable used to identify the ID of the selected Paper
-let selectedPaper = "0";
+let selectedPaper = '0';
 
 /**
  * This method generates the graph for the provided graphsAndSimilarities Object
  * @param data contains all papers, similarities and similarities between papers
  * @returns a GraphData object consisting of nodes[] and links[]
  */
-const genGraph = (data:papersAndSimilarities):myGraphData =>{
+const generateGraph = (data : PapersAndSimilarities) : MyGraphData =>{
     var i,j,s;
     var links = [];
     var nodes = [];
-    var simMat:number[][] = new Array(data.paper.length);
+    var simMat : number[][] = new Array(data.paper.length);
     selectedPaper = data.paper[0].id;
     data.tensor = data.tensor.map((x) => x.map((y) => y.map((z) => (z < 0)? 0 : z)));
     // For now we only use the very first similarity tensor[0] 
@@ -150,26 +153,37 @@ const genGraph = (data:papersAndSimilarities):myGraphData =>{
         }
         simMat[i] = tempArray;
     }
-    console.log(simMat);
-    var boundary = find_boundary(simMat);
-    console.log(boundary);
+    //console.log(simMat);
+    const boundary = FindBoundary(simMat);
+    //console.log(boundary);
     for (i = 0; i < data.paper.length; i++){
         for (j = i+1; j < data.paper.length; j++){
-            //if(i == 0){
-            if(simMat[i][j] > boundary){ //Threshhold for generating Links
+            if(i == 0){
+            //if(simMat[i][j] > boundary){ //Threshhold for generating Links
                 links.push({
                     source: data.paper[i].id,
                     target: data.paper[j].id,
-                    color: "#FFFFFF",
+                    color: '#FFFFFF',
                     similarity: simMat[i][j],
-                    label: simMat[i][j].toString(),
+                    label: (1000/simMat[i][j]).toString(),
             })/*}*/}
+            else{
+                if(simMat[i][j] > 27.5){
+                    links.push({
+                        source: data.paper[i].id,
+                        target: data.paper[j].id,
+                        color: '#FFFFFF',
+                        similarity: simMat[i][j],
+                        label: simMat[i][j].toString(),
+                    })
+                }
+            }
         }
         //Create Nodes for every Paper in data.paper
-        var id = data.paper[i];
+        const id = data.paper[i];
         nodes.push({
             id: id.id,
-            title: id.title,//"Number of o´s in Name: " + (id.title.split("o").length-1) + "\nNumber of s in Name: " + (id.title.split("s").length-1),
+            title: id.title,
             paperAbstract: id.paperAbstract,
             authors: id.authors,
             inCitations: id.inCitations,
@@ -189,66 +203,76 @@ const genGraph = (data:papersAndSimilarities):myGraphData =>{
             magId:id.magId,
             s2PdfUrl: id.s2PdfUrl,
             entities: id.entities,
-            color: "",
-            originSim: data.tensor[0][0][i],
-        })
+            color: '',
+            originSim: simMat[0][i],
+        });
     }
     // Fix Position of the selected Paper in the center of the canvas
-    (nodes[0] as NodeObject).fx = 0;
-    (nodes[0] as NodeObject).fy = 0;
+    (nodes[0] as Paper).fx = 0;
+    (nodes[0] as Paper).fy = 0;
     
     return ({    
             nodes: nodes,
-            links: links
+            links: links,
         }
     );
 }
 
-const check_connections = (mat:number[][], thr:number) => {
-    var mat_c = JSON.parse(JSON.stringify(mat));
-    mat_c = mat_c.map((x:number[]) => x.map(z => z>thr ? z : -1));
-    let x:Set<number> = new Set();
+/**
+ * Returns true if the provided threshold for Link Generation results in a fully connected Graph. In other Words, that no node ends up without a link
+ * @param mat contains the Link-value for each Pair of Nodes
+ * @param thr is the threshold to determine if the link will be included in the graph or not
+ */
+const CheckConnections = (matrix : number[][], threshold : number) => {
+    var matrix_c = JSON.parse(JSON.stringify(matrix));
+    matrix_c = matrix_c.map((x : number[]) => x.map(z => z>threshold ? z : -1));
+    let x : Set<number> = new Set();
     x.add(0);
-    for (let i = 0; i<mat_c.length; i++) {
+    for (let i = 0; i<matrix_c.length; i++) {
       for (let val of [...Array.from(x)]) {
-        for (let k = 0; k < mat_c.length; k++) {
-          if (mat_c[val][k] > -1) {
-            x.add(k)
-            if (x.size == mat_c.length) {
-              return true
+        for (let k = 0; k < matrix_c.length; k++) {
+          if (matrix_c[val][k] > -1) {
+            x.add(k);
+            if (x.size == matrix_c.length) {
+              return true;
             }
           }
         }
       }
     }
-    return false
+    return false;
  };
  
-const  find_boundary = (mat:number[][]) => {
-   var mat_c2 = JSON.parse(JSON.stringify(mat));
-   var max_m = Math.max(...mat_c2.map((x:number[]) => Math.max(...x)))
+ /**
+  * Function to determine the smallest threshhold for Link Generation so that every Node ist still connected.
+  */
+const  FindBoundary = (matrix : number[][]) => {
+   var matrixC2 = JSON.parse(JSON.stringify(matrix));
+   const maxOfMatrix = Math.max(...matrixC2.map((x : number[]) => Math.max(...x)));
  
-   let v_top = max_m
-   let v_bottom = 0
+   let upperBound = maxOfMatrix;
+   let lowerBound = 0;
  
    for (let i = 0; i < 10; i++) {
-     let mid = (v_top + v_bottom) / 2
-     let bo = check_connections(mat, mid)
+     let mid = (upperBound + lowerBound) / 2;
+     let bo = CheckConnections(matrix, mid);
      if (bo) {
-       v_bottom = mid
+        lowerBound = mid;
      } else {
-       v_top = mid
+        upperBound = mid;
      }
    }
-   
-   return v_bottom
+   return lowerBound;
 }
 
+/**
+ * Helperfunction to fetch Graph Data during Development. Will be deleted in later Versions
+ */
 export const GraphFetch: React.FC = () => {
     /*
     ** useState Hook to save the graphData 
     */
-    const [graph, setGraph] = React.useState<papersAndSimilarities>({tensor: [[[]]], paper: [], similarities: []});
+    const [graph, setGraph] = React.useState<PapersAndSimilarities>({tensor: [[[]]], paper: [], similarities: []});
 
     /*
     ** EffectHook for the initial Load of the graph
@@ -261,50 +285,50 @@ export const GraphFetch: React.FC = () => {
     ** loadData fetches the graph_Data from the backend and saves the generated Graph in the State Hook graph
     */
     const loadData = () => {
-        fetch(Config.base_url + "/api/generate_graph/?paper_id=1ae8584e12459279ee915f4cda5c552c14697b07")
+        fetch(Config.base_url + '/api/generate_graph/?paper_id=1ae8584e12459279ee915f4cda5c552c14697b07')
             .then(res => res.json())
             .then(res => {setGraph(res);
                             return res});
-    }
+    };
 
     return (
         <Graph data={graph}/>
-    )
+    );
 }
 /**
  * TODO: delete + set correct origin node
  */
 const initNode = {
-    id: "0",
-    title: "Origin",
-    paperAbstract: "",
-    authors: [{name: "John Glanz", "ids":["321534234"]}],
-    inCitations: ["asdasd", "aisdingk"],
-    outCitations: ["fadg"],
+    id: '0',
+    title: 'Origin',
+    paperAbstract: '',
+    authors: [{name: 'John Glanz', 'ids':['321534234']}],
+    inCitations: ['asdasd', 'aisdingk'],
+    outCitations: ['fadg'],
     year: 2021,
-    s2Url: "",
-    sources: [""],
-    pdfUrls: [""],
-    venue: "",
-    journalName: "",
-    journalVolume: "",
-    journalPages: "",
-    doi: "",
-    doiUrl: "",
-    pmid: "",
-    fieldsOfStudy: ["Materials Science"],
-    magId: "",
-    s2PdfUrl: "",
-    entities: [""],
+    s2Url: '',
+    sources: [''],
+    pdfUrls: [''],
+    venue: '',
+    journalName: '',
+    journalVolume: '',
+    journalPages: '',
+    doi: '',
+    doiUrl: '',
+    pmid: '',
+    fieldsOfStudy: ['Materials Science'],
+    magId: '',
+    s2PdfUrl: '',
+    entities: [''],
 
-    color: "",
-}
+    color: '',
+};
 
 /**
  * main Method for generating the Graph
  * @returns everything that is displayed under the Graph Tab
  */
-export const Graph: React.FC<{"data": papersAndSimilarities}> = (props) => {
+export const Graph: React.FC<{'data' : PapersAndSimilarities}> = (props) => {
     /**
     ** Reference to the Graph used for TODO: insert Usage
     */
@@ -325,33 +349,42 @@ export const Graph: React.FC<{"data": papersAndSimilarities}> = (props) => {
     ** selected Node to display on drawer
     */
     const[selectedNode, setNode] = React.useState(initNode);
+
+    //Load an empty Graph until the real Data is fetched
+    const myGraphData = (props.data.paper.length == 0)? ({nodes : [], links : []}) : generateGraph(props.data);
+
     /*
-    ** EffectHook for the initial Load of the graph
+    ** EffectHook for playing with forces
     */
     React.useEffect(() => {
-            const fg:any = fgRef.current;
+            const fg : any = fgRef.current;
             //Playing with the forces on the graph
-            //fg.d3Force('center', null);
-            //fg.d3Force('link', null);
+            fg.d3Force('link', null);
             //fg.d3Force('charge', null);
-            fg.d3Force('center', null);
-            //fg.d3Force('radial', forceRadial(30));
-            //fg.d3Force('radial').radius((node:paper) => (node.originSim)*10);
+            //fg.d3Force('center', null);
+            fg.d3Force('charge').strength((node : NodeObject) => -3);
+            fg.d3Force('radial', forceRadial(30));
+            fg.d3Force('radial').radius((node : Paper) => 1000/(node.originSim));
             //console.log(props.data.links.filter((link:myLinkObject) => ((link.source as NodeObject).id != props.data.nodes[0].id)));
-            //let links = props.data.links.filter((link:myLinkObject) => ((link.source as NodeObject).id != props.data.nodes[0].id));
-            //fg.d3Force('link').iterations(1).links(props.data.links.filter((link:myLinkObject) => ((link.source as NodeObject).id != props.data.nodes[0].id)));
-            fg.d3Force('link').iterations(1).distance((link:myLinkObject) => link.similarity *5);
-            fg.d3Force('link').iterations(1).strength((link:myLinkObject) => 1/ link.similarity);
+            //let links = myGraphData.links.filter((link : myLinkObject) => (link.source != myGraphData.nodes[0].id));
+            //fg.d3Force('link').links(links);
+            //{console.log(link.source);
+              //  console.log(myGraphData.nodes[0].id);
+                //return (link.source != myGraphData.nodes[0].id)}));
+            //fg.d3Force('link').iterations(1).distance((link : myLinkObject) => link.similarity *5);
+            //fg.d3Force('link').iterations(1).strength((link : myLinkObject) => 1/ link.similarity);
         },[]);
+
+    /**
+     * EffectHook for rerendering upon slider changes
+     */
     React.useEffect(() => {
-        const fg:any = fgRef.current;
+        const fg : any = fgRef.current;
         //fg.d3Force("link").iterations(1).distance((link:myLinkObject) => (link.similarity[0] * firstSliderValue/10 + link.similarity[1] * secondSliderValue/10 + link.similarity[2] * 5));//link.similarity.reduce(((x,y) => x + y), 0)*sliderValue));
         fg.d3ReheatSimulation();
     },[firstSliderValue, secondSliderValue]);
 
     //console.log(props.data);
-
-    const myGraphData = (props.data.paper.length == 0)? ({nodes : [], links : []}) : genGraph(props.data);
 
     return(
         <div>
@@ -405,7 +438,7 @@ export const Graph: React.FC<{"data": papersAndSimilarities}> = (props) => {
             <Drawer
                 show={drawer}
                 backdrop={false}
-                onHide={() => {setDrawer(false)}}
+                onHide={() => {setDrawer(false);}}
             >
                 <Drawer.Header>
                     <Drawer.Title>
@@ -414,11 +447,11 @@ export const Graph: React.FC<{"data": papersAndSimilarities}> = (props) => {
                 </Drawer.Header>
                 <Drawer.Body>
                     <p>
-                        <Button color="cyan" appearance="ghost" href={selectedNode.s2Url} target="_blank">
+                        <Button color='cyan' appearance='ghost' href={selectedNode.s2Url} target='_blank'>
                             Open in Semantic Scholar
                         </Button>
                     </p>
-                    <p style={{color:"grey"}}>{selectedNode.year}{selectedNode.authors.map(author => <>, {author.name}</>)}
+                    <p style={{color:'grey'}}>{selectedNode.year}{selectedNode.authors.map(author => <>, {author.name}</>)}
                         <br/> Citations: {selectedNode.inCitations.length}, References: {selectedNode.outCitations.length}
                         <br/><p style={{color:selectedNode.color}}>Field: {selectedNode.fieldsOfStudy.map(field => <> {field}</>)} </p>
                     </p>
@@ -437,23 +470,23 @@ export const Graph: React.FC<{"data": papersAndSimilarities}> = (props) => {
                           onNodeClick={(node, e) => {
                               e.preventDefault();
                               if (node.id === selectedNode.id) {
-                                  setDrawer(!drawer)
+                                  setDrawer(!drawer);
                               } else {
-                                  setNode((node as paper))
-                                  setDrawer(true)
-                              }
+                                  setNode((node as Paper));
+                                  setDrawer(true);
+                              };
                           }}
                           onBackgroundClick={(e) => {
                               e.preventDefault()
                               setDrawer(false)
                           }}
-                          nodeAutoColorBy="fieldsOfStudy"
-                          nodeLabel="title"
-                          linkLabel={(link:any) =>(link.label)}
-                          linkWidth={(link:any) =>(link.similarity/5)}
-                          linkCurvature="curvature"
-                          linkDirectionalArrowLength="arrowLen"
-                          linkDirectionalParticles="dirParticles"
+                          nodeAutoColorBy='fieldsOfStudy'
+                          nodeLabel='title'
+                          linkLabel={(link) =>((link as MyLinkObject).label)}
+                          linkWidth={(link) =>((link as MyLinkObject).similarity/5)}
+                          linkCurvature='curvature'
+                          linkDirectionalArrowLength='arrowLen'
+                          linkDirectionalParticles='dirParticles'
                           //Add this line together with the initialising and instantiating of selectedPaper to show only Links connected to the selectetPaper
                           //linkVisibility={(link:LinkObject) => ((link.source as NodeObject).id == selectedPaper)}
                           d3VelocityDecay={0.4}
