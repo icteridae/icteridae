206a81b05efc79fcba61bfbf9cb461d4e93b2274
joynere <nicokunznk00@gmail.com>
2021-02-11 00:22:39 +0100

    Add file for types of TreeNodes

---
+++ frontend/src/Components/SavedPapers/TreeTypes.tsx
@@ -0,0 +1,23 @@
+export interface TreeNode {
+    label: JSX.Element;
+    value: String;
+}
+
+export interface PaperNode extends TreeNode {
+    paperId: String;
+}
+
+export interface DirectoryNode extends TreeNode {
+    children: PaperOrDirectoryNode[];
+    directoryName: String;
+}
+
+export type PaperOrDirectoryNode = DirectoryNode | PaperNode;
+
+export function isPaperNode(node: TreeNode | undefined): node is PaperNode {
+    return node !== undefined && (node as PaperNode).paperId !== undefined;
+}
+
+export function isDirectoryNode(node: TreeNode | undefined): node is DirectoryNode {
+    return node !== undefined && (node as DirectoryNode).children !== undefined;
+}
