73cd775022365ae5713939f042ec8b7217d1160a
lennart.mischnaewski <lennart.mischnaewski@stud.tu-darmstadt.de>
2021-02-12 13:28:09 +0100

    Fix start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 trap 'kill %1' SIGINT
-<<<<<<< HEAD
 ./backend/manage.py runserver & yarn --cwd frontend/ start
-=======
-./backend/manage.py runserver & yarn --cwd frontend/ start
->>>>>>> feature
