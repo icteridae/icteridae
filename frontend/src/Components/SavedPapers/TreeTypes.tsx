07c77042a4bf61c1069a9c1dd3a27915c71e1e29
Inluminous <lenmisch@gmail.com>
2021-02-14 00:11:35 +0100

    Add stripped tree types

--- frontend/src/Components/SavedPapers/TreeTypes.tsx
+++ frontend/src/Components/SavedPapers/TreeTypes.tsx
@@ -1,6 +1,11 @@
-export interface TreeNode {
+export interface BaseTreeNode {
+    children?: BaseTreeNode[];
+}
+
+export interface TreeNode extends BaseTreeNode{
     label: JSX.Element;
     value: string;
+    children?: PaperOrDirectoryNode[];
 }
 
 export interface PaperNode extends TreeNode {
@@ -19,17 +24,25 @@ export function isPaperNode(node: TreeNode | undefined): node is PaperNode {
 }
 
 export function isDirectoryNode(node: TreeNode | undefined): node is DirectoryNode {
-    return node !== undefined && (node as DirectoryNode).children !== undefined;
+    return node !== undefined && (node as DirectoryNode).directoryName !== undefined;
 }
 
-export interface StrippedPaperNode {
+export function hasChildren(node: BaseTreeNode | undefined) {
+    return node !== undefined && node.children !== undefined
+}
+
+export interface StrippedPaperNode extends BaseTreeNode{
     paperId: string;
 }
 
-export interface StrippedDirectoryNode {
+export interface StrippedDirectoryNode extends BaseTreeNode{
     directoryName: string;
-    children: PaperOrDirectoryNode[];
+    children: StrippedPaperOrDirectoryNode[];
     value: string;
+}
+
+export function isStrippedDirectoryNode(node: BaseTreeNode | undefined): node is StrippedDirectoryNode {
+    return node !== undefined && (node as DirectoryNode).directoryName !== undefined && (node as DirectoryNode).value !== undefined;
 }
 
 export type StrippedPaperOrDirectoryNode = StrippedPaperNode | StrippedDirectoryNode;
