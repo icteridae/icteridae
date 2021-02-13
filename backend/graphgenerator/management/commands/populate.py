1c06ed09a6b19e8a9c6c1cda4d37b278bfb786db
Inluminous <lenmisch@gmail.com>
2021-02-13 12:18:14 +0100

    Add files argument for populate

--- backend/graphgenerator/management/commands/populate.py
+++ backend/graphgenerator/management/commands/populate.py
@@ -8,9 +8,10 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-l', '--limit', type=int, help='Limit number of lines read from each file.')
         parser.add_argument('-b', '--batch', type=int, help='Number of papers per batch. Increase for faster runtime with more RAM usage.')
+        parser.add_argument('-f', '--files', type=str, nargs='+', help='Files to process. Defaults to all.')
 
     def handle(self, *args, **options):
-        load(options['limit'], options['batch'] if options['batch'] else 1000, options['verbosity'])
+        load(options['limit'], options['batch'] if options['batch'] else 1000, options['verbosity'], options['files'])
 
         call_command('search_index', '--rebuild', '-f', '--verbosity', options['verbosity'])
         if options['verbosity'] > 1: self.stdout.write('Done populating.')
