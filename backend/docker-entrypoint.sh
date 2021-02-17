324134b886ab6bc69d5a504ba6606afde257a81a
lennart.mischnaewski <lennart.mischnaewski@stud.tu-darmstadt.de>
2021-02-17 20:06:52 +0100

    Fix inital docker bug

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+while ! curl http://db:5432 2>&1 | grep '52'
+do
+    echo 'Waiting for postgres'
+    sleep 1
+done
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
