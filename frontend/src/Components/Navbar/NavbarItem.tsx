483808993959cf259553e7c905ce66b28ca9a52a
joynere <nicokunznk00@gmail.com>
2021-02-25 23:37:59 +0100

    Add navbar showing current link

--- frontend/src/Components/Navbar/NavbarItem.tsx
+++ frontend/src/Components/Navbar/NavbarItem.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import {Icon, IconProps} from 'rsuite';
 
 export const NavbarItem : React.FC<{icon?: IconProps["icon"], label?: string, path: string, extern?: boolean, className?: string}> = (props) => {
     let history = useHistory();
     return (
         <div onClick={(props.extern) ? () => {window.open(props.path)} : () => {history.push(props.path)}} className={"navbar-item " + (props.className || '')}>
-            <span> 
-                {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
-                {props.label}
-            </span>
+            <NavLink exact to={props.path} activeClassName={"navbar-item-active"}>
+                <span> 
+                    {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
+                    {props.label}
+                </span>
+            </NavLink>
         </div>
     )
 }
