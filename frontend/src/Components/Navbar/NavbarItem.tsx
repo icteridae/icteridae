dc4b253a249c69006a75ef8d7c36f7c4c07d2a2b
joynere <nicokunznk00@gmail.com>
2021-02-26 15:52:07 +0100

    Fix style of navbar-items

--- frontend/src/Components/Navbar/NavbarItem.tsx
+++ frontend/src/Components/Navbar/NavbarItem.tsx
@@ -4,22 +4,22 @@ import {Icon, IconProps} from 'rsuite';
 
 export const NavbarItem : React.FC<{icon?: IconProps["icon"], label?: string, path: string, extern?: boolean, className?: string}> = (props) => {
     return (
-        <a href={props.path} target={(props.extern) ? "_blank" : undefined} rel={"noreferrer"} className={"navbar-item " + (props.className || '')}>
-            {(props.extern) ? 
-            (
-                <span> 
-                    {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
-                    {props.label}
-                </span>
-            ) : 
-            (
-                <NavLink exact to={props.path} activeClassName={"navbar-item-active"}>
+        <>
+            {(props.extern) ? (
+                <a href={props.path} target={"_blank"} rel={"noreferrer"} className={"navbar-item " + (props.className || '')}>
                     <span> 
                         {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
                         {props.label}
-                    </span>
+                    </span> 
+                </a>
+            ) : (
+                <NavLink exact to={props.path} className={"navbar-item " + (props.className || '')} activeClassName={"navbar-item-active"}> 
+                        <span> 
+                            {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
+                            {props.label}
+                        </span>
                 </NavLink>
-            )} 
-        </a>
+            )}
+        </>
     )
 }
