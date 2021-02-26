09994d42b940704674ffeba2a2d5d92d49571338
joynere <nicokunznk00@gmail.com>
2021-02-26 15:32:49 +0100

    Fix problems with extern links

--- frontend/src/Components/Navbar/NavbarItem.tsx
+++ frontend/src/Components/Navbar/NavbarItem.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import {Icon, IconProps} from 'rsuite';
 
 export const NavbarItem : React.FC<{icon?: IconProps["icon"], label?: string, path: string, extern?: boolean, className?: string}> = (props) => {
-    let history = useHistory();
-
     return (
-        <div onClick={(props.extern) ? () => {} : () => {history.push(props.path)}} className={"navbar-item " + (props.className || '')}>
+        <a href={props.path} target={(props.extern) ? "_blank" : undefined} rel={"noreferrer"} className={"navbar-item " + (props.className || '')}>
             {(props.extern) ? 
             (
-                <a href={props.path} target="_blank" rel="noreferrer">
                 <span> 
                     {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
                     {props.label}
                 </span>
-                </a>
             ) : 
             (
                 <NavLink exact to={props.path} activeClassName={"navbar-item-active"}>
@@ -24,6 +20,6 @@ export const NavbarItem : React.FC<{icon?: IconProps["icon"], label?: string, pa
                     </span>
                 </NavLink>
             )} 
-        </div>
+        </a>
     )
 }
