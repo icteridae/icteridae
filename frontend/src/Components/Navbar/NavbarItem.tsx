48f6714956283ff67a2673bd613f68a78925028e
joynere <nicokunznk00@gmail.com>
2021-02-26 15:00:28 +0100

    Fix github link appending to current url on click

--- frontend/src/Components/Navbar/NavbarItem.tsx
+++ frontend/src/Components/Navbar/NavbarItem.tsx
@@ -4,9 +4,10 @@ import {Icon, IconProps} from 'rsuite';
 
 export const NavbarItem : React.FC<{icon?: IconProps["icon"], label?: string, path: string, extern?: boolean, className?: string}> = (props) => {
     let history = useHistory();
+
     return (
         <div onClick={(props.extern) ? () => {window.open(props.path)} : () => {history.push(props.path)}} className={"navbar-item " + (props.className || '')}>
-            <NavLink exact to={props.path} activeClassName={"navbar-item-active"}>
+            <NavLink exact to={(props.extern) ? "" : props.path} activeClassName={(props.extern) ? "" : "navbar-item-active"}>
                 <span> 
                     {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
                     {props.label}
