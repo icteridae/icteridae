db58639d267f2281de1ed128dd763418920580db
joynere <nicokunznk00@gmail.com>
2021-02-26 15:10:31 +0100

    Fix application going back to the start page when clicking on extern links

--- frontend/src/Components/Navbar/NavbarItem.tsx
+++ frontend/src/Components/Navbar/NavbarItem.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import {Icon, IconProps} from 'rsuite';
 
 export const NavbarItem : React.FC<{icon?: IconProps["icon"], label?: string, path: string, extern?: boolean, className?: string}> = (props) => {
-    let history = useHistory();
-
     return (
-        <div onClick={(props.extern) ? () => {window.open(props.path)} : () => {history.push(props.path)}} className={"navbar-item " + (props.className || '')}>
-            <NavLink exact to={(props.extern) ? "" : props.path} activeClassName={(props.extern) ? "" : "navbar-item-active"}>
+        <div className={"navbar-item " + (props.className || '')}>
+            {(props.extern) ? 
+            (
+                <a href={props.path} target="_blank" rel="noreferrer">
                 <span> 
                     {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
                     {props.label}
                 </span>
-            </NavLink>
+                </a>
+            ) : 
+            (
+                <NavLink exact to={props.path} activeClassName={"navbar-item-active"}>
+                    <span> 
+                        {props.icon && <Icon icon={props.icon} style={{paddingRight: "5px", fontSize: "15px"}}/>}
+                        {props.label}
+                    </span>
+                </NavLink>
+            )} 
         </div>
     )
 }
