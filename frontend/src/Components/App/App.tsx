816d87b505057f8309f1a7dea264da65496a586e
Inluminous <lenmisch@gmail.com>
2021-03-12 12:34:12 +0100

    Add description

--- frontend/src/Components/App/App.tsx
+++ frontend/src/Components/App/App.tsx
@@ -9,6 +9,7 @@ import { GraphFetch } from '../Graph/GraphHelperfunctions';
 import { PageSavedPapers } from '../SavedPapers/PageSavedPapers';
 import { PageSearchResult } from '../Search/SearchResult/PageSearchResult';
 import { SavedPapers } from '../SavedPapers/PageSavedPapersLocalstorage';
+import { Description } from '../Description/Description';
 
 import './App.scss';
 
@@ -22,14 +23,15 @@ import logo from '../../icon.png'
           <Route exact path='/results/:query' component={PageSearchResult}/>
           <Route exact path='/privacy' component={PageImprint}/>
           <Route exact path ='/papers' component={SavedPapers}/>
+          <Route exact path='/description' component={Description}/>
           <Route exact path='/graph/:id' component={GraphFetch}/>
           <Route exact path='/results/paper/:id' component={FrontPage}/>
           {/* TODO: insert other routes. See paths in Navbar.tsx */}
       </Switch>
       
-      <Footer className='footer'>
+      {/*<Footer className='footer'>
                 <img src={logo} alt="Logo"/> &copy; {new Date().getFullYear()} Icteridae
                 <Button className='impressum' onClick={() => Alert.info('There is no impressum yet', 5000)} appearance='link'>Impressum</Button>
-      </Footer>
+ </Footer>*/}
     </BrowserRouter>
   );
