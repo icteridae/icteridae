c1895c0afe152a868ada387b781a4ca154f898a3
Inluminous <lenmisch@gmail.com>
2021-03-13 23:07:48 +0100

    Add related authors

--- backend/graphgenerator/views.py
+++ backend/graphgenerator/views.py
@@ -105,7 +105,6 @@ def generate_graph(request):
                               'paper': PaperSerializer(relevant, many=True).data,
                               'similarities': similarities})
 
-
 @api_view(['GET'])
 def get_paper(request):
     """
@@ -125,8 +124,6 @@ def get_paper(request):
         return http.JsonResponse(PaperSerializer(paper).data)
     except:
         return http.HttpResponseBadRequest('Paper not found')
-
-
 
 @api_view(['POST'])
 def get_paper_bulk(request):
@@ -269,5 +266,39 @@ def get_authorpapers(request):
                                     many=True).data,
             'max_pages': max_pages,
             'count': count
+        },
+        safe=False)
+
+@api_view(['GET'])
+def get_author_details(request):
+    """
+    returns papers written by author based on id
+
+    request needs to have 'author_id':any field
+    """
+
+    author_id = request.query_params.get('author_id', None)
+    if author_id is None:
+        return http.HttpResponseBadRequest('no query supplied.')
+
+    results = Author.objects.raw(f"""SELECT a2.id, a2.name FROM 
+	graphgenerator_author as a1 
+	JOIN graphgenerator_authorpaper as ap1 
+	ON a1.id = ap1.author_id 
+	
+	JOIN graphgenerator_authorpaper as ap2
+	ON ap1.paper_id = ap2.paper_id AND NOT ap2.author_id = a1.id
+	
+	JOIN graphgenerator_author as a2
+	ON ap2.author_id = a2.id
+		WHERE a1.id = '{author_id}'
+		GROUP BY a2.id
+		ORDER BY COUNT(*) DESC
+		LIMIT 10;""")
+
+    return http.JsonResponse(
+        {
+            'data': AuthorSerializer(results,
+                                    many=True).data,
         },
         safe=False)
--- frontend/src/Components/Author/AuthorSearchResult.tsx
+++ frontend/src/Components/Author/AuthorSearchResult.tsx
@@ -5,7 +5,8 @@ import {Paper} from "../../Utils/GeneralTypes";
 import {AuthorInterface} from "./AuthorInterface";
 import {SearchResultCard} from "../Search/SearchResult/SearchResultCard";
 import {AutoComplete, Icon, InputGroup} from "rsuite";
-import {useHistory, useParams} from "react-router-dom";
+import {Link, useHistory, useParams} from "react-router-dom";
+import { AuthorCard } from './AuthorSearch';
 
 interface AuthorResultProps {
     text?: string;
@@ -24,17 +25,19 @@ export const AuthorSearchResult: React.FC<AuthorResultProps> = (props) => {
     const [authorPapers, setAuthorPapers] = useState<Paper[]>();
     // Publication count
     const [publications, setPublications] = useState<number>();
+    // Related authors
+    const [relatedAuthors, setRelatedAuthors] = useState<AuthorInterface[]>([]);
 
 
     // Effect hook for setting author and fetching paper data from search API
     useEffect(() => {
-        let requestURLAuthor = Config.base_url + '/api/author/?author_id=' + id;
+        const requestURLAuthor = Config.base_url + '/api/author/?author_id=' + id;
 
         fetch(requestURLAuthor)
             .then(res => res.json())
             .then(result => setSelectedAuthor(result.data[0])).catch(() => console.log("Can't access " + requestURLAuthor));
 
-        let requestURLAuthorPapers = Config.base_url + '/api/authorpapers/?author_id=' + id;
+        const requestURLAuthorPapers = Config.base_url + '/api/authorpapers/?author_id=' + id;
 
         fetch(requestURLAuthorPapers)
             .then(res => res.json())
@@ -42,6 +45,15 @@ export const AuthorSearchResult: React.FC<AuthorResultProps> = (props) => {
                 setAuthorPapers(result.data);
                 setPublications(result.count);
             }).catch(() => console.log("Can't access " + requestURLAuthorPapers));
+
+        const requestURLAuthorDetails = Config.base_url + '/api/author_details/?author_id=' + id;
+
+        fetch(requestURLAuthorDetails)
+            .then(res => res.json())
+            .then(results => {
+                setRelatedAuthors(results.data);
+            }).catch(() => console.log("Can't access " + requestURLAuthorDetails));
+
     }, [id]);
 
     const buttonClick = () => {
@@ -56,7 +68,16 @@ export const AuthorSearchResult: React.FC<AuthorResultProps> = (props) => {
             <div className="author-search-result">
                 <div className="author-details">
                     <div className="author-name">{selectedAuthor.name}</div>
-                    <div><b>Publications: </b>{publications}</div>
+                    <div className="author-sub">
+                        <div className="author-pub">
+                            {publications} publications</div>
+                        <div className="author-related">
+                            <div className='author-rel-title'>Related authors</div>
+                            {relatedAuthors.map(author => 
+                                <AuthorCard author={author}/>
+                            )}
+                        </div>
+                    </div>
                 </div>
                 <div className="paper-list">
                     <div className="publications">Publications</div>
