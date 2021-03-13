93d0ef7e5315982f7a2090de0ebdd6279ebb57de
joynere <nicokunznk00@gmail.com>
2021-03-13 23:23:41 +0100

    Change names of authors in Authors to be a link to their own Page

--- frontend/src/Components/General/Authors.tsx
+++ frontend/src/Components/General/Authors.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export const Authors : React.FC<{authors: {name: string, id: string}[], maxAuthors: number}> = (props) => {
     return(
         <span className="authors">
@@ -7,10 +9,10 @@ export const Authors : React.FC<{authors: {name: string, id: string}[], maxAutho
                 // display a maximum of maxAuthors, if number of authors is higher than maxAuthors display how many other authors contributed
                 props.authors
                     .slice(0, props.maxAuthors)
-                    .map(author => author.name)
-                    .join(', ') + 
-                        ((props.authors.length > props.maxAuthors) ? `, + ${props.authors.length - props.maxAuthors} others` : "")
+                    .map<React.ReactNode>(author => (<Link to={`/author/${author.id}`}>{author.name}</Link>))
+                    .reduce((prev, curr) => [prev, ', ', curr])
             }
+            {((props.authors.length > props.maxAuthors) ? `, + ${props.authors.length - props.maxAuthors} others` : "")}
         </span>
     );
 }
