1fe0ef087e5eb82fb95dab9ddb88b50c047c56ce
joynere <nicokunznk00@gmail.com>
2021-03-13 12:25:37 +0100

    Extract code displaying authors into new component

---
+++ frontend/src/Components/General/Authors.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Authors : React.FC<{authors: {name: string, ids: number[]}[], maxAuthors: number}> = (props) => {
+    return(
+        <span className="authors">
+            {
+                // display a maximum of maxAuthors, if number of authors is higher than maxAuthors display how many other authors contributed
+                props.authors
+                    .slice(0, props.maxAuthors)
+                    .map(author => author.name)
+                    .join(', ') + 
+                        ((props.authors.length > props.maxAuthors) ? `, + ${props.authors.length - props.maxAuthors} others` : "")
+            }
+        </span>
+    );
+}
