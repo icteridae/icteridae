f215875da8e49ab9a70effa18b26757710760a3c
joynere <nicokunznk00@gmail.com>
2021-03-15 13:54:57 +0100

    Add working delete  button appearing next to the selected tree-node

--- frontend/src/Components/SavedPapers/SavedPapersTree.tsx
+++ frontend/src/Components/SavedPapers/SavedPapersTree.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Button, Loader, Tree } from 'rsuite';
+import { Button, Loader, Tree, Icon} from 'rsuite';
 import { DropData } from 'rsuite/lib/TreePicker';
 
 import * as TreeTypes from './TreeTypes';
@@ -44,29 +44,52 @@ export const SavedPapersTree: React.FC<{setSelectedPaper: Function}> = (props) =
         setTreeData(treeData => PaperFunctions.deepMap(treeData, 
             node => (
                 TreeTypes.isPaperNode(node) && loadedPapers.hasOwnProperty(node.paperId) ? 
-                    {...node, label: 
-                        <>{loadedPapers[node.paperId].title 
-                            + ' (' 
-                            + loadedPapers[node.paperId].authors[0].name.split(' ').slice(-1)[0] 
-                            + ' ' 
-                            + loadedPapers[node.paperId].year 
-                            + ')'} </>} 
+                    {...node, 
+                    label: (
+                        <div className="tree-node">
+                            {loadedPapers[node.paperId].title 
+                                + ' (' 
+                                + loadedPapers[node.paperId].authors[0].name.split(' ').slice(-1)[0] 
+                                + ' ' 
+                                + loadedPapers[node.paperId].year 
+                                + ')'
+                            }
+                            {(node.paperId == selectedTreeNode?.value) &&
+                                <button className='delete-button' onClick={() => setTreeData(PaperFunctions.deleteTreeNode(selectedTreeNode.value, treeData))}>
+                                    <Icon icon='trash'/>
+                                </button>
+                            }
+                        </div>
+                    ),
+                        } 
                 : node
                 )))
-    }, [loadedPapers])
+    }, [loadedPapers, selectedTreeNode])
 
     useEffect(() => {
         setTreeData(treeData => PaperFunctions.deepMap(treeData,
             node => (
                 TreeTypes.isDirectoryNode(node) && directoryNames.hasOwnProperty(node.value) ? {
                     ...node, 
-                    label: <RenamableDirectory name={directoryNames.hasOwnProperty(node.value) ? directoryNames[node.value] : 'Loading...'} 
-                                setName={(val) => setDirectoryNames((directoryNames) => ({...directoryNames, [node.value]: val}))}/>,
+                    label: (
+                        <div className="tree-node">
+                            <RenamableDirectory 
+                                name={directoryNames.hasOwnProperty(node.value) ? directoryNames[node.value] : 'Loading...'} 
+                                setName={(val) => setDirectoryNames((directoryNames) => ({...directoryNames, [node.value]: val}))}
+                            />
+                            {(node.value == selectedTreeNode?.value) && 
+                                <button className='delete-button' onClick={() => setTreeData(PaperFunctions.deleteTreeNode(selectedTreeNode.value, treeData))}>
+                                    <Icon icon='trash'/>
+                                </button>
+                            }
+                        </div>
+                    ),
                     directoryName: directoryNames[node.value]
             } 
                 : node
-            )))
-    }, [directoryNames])
+            )));
+            console.log(selectedTreeNode);
+    }, [directoryNames, selectedTreeNode])
 
     useEffect(() => {
         localStorage.setItem('savedpapers', JSON.stringify(PaperFunctions.deepMap(PaperFunctions.stripTree(treeData), (node) => (TreeTypes.isStrippedDirectoryNode(node) ? {...node, directoryName: directoryNames[node.value]} : node))))
@@ -89,11 +112,6 @@ export const SavedPapersTree: React.FC<{setSelectedPaper: Function}> = (props) =
                 <Button onClick={() => localStorage.setItem('savedpapers', JSON.stringify([]))}>
                     Reset localStorage
                 </Button>
-                {selectedTreeNode != null && (
-                    <Button onClick={() => setTreeData(PaperFunctions.deleteTreeNode(selectedTreeNode.value, treeData))}>
-                        Delete
-                    </Button>
-                )}
                 {TreeTypes.isPaperNode(selectedTreeNode) && (
                     <Link to={`/graph/${selectedTreeNode.paperId}`}>Load Graph</Link>
                 )}
