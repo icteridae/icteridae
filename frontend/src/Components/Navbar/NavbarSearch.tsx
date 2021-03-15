fbdd7e0d1a9a8e6661de3adb78c6d5ddbdbc024f
Inluminous <lenmisch@gmail.com>
2021-03-15 12:06:47 +0100

    Fix bug when confirming empty search

--- frontend/src/Components/Navbar/NavbarSearch.tsx
+++ frontend/src/Components/Navbar/NavbarSearch.tsx
@@ -11,7 +11,7 @@ export const NavbarSearch : React.FC<{label: string, value: string, raiseStateIn
     // focus input if user clicks anywhere on the element
     return (
         <div className={`navbar-search navbar-item ${props.className}`} onClick={() => {(document.querySelector(`.navbar-search.${props.className} input[type=text]`) as HTMLInputElement).focus()}}>
-            <form id="navbar-search-form" onSubmit={(e) => {e.preventDefault(); history.push(`/${props.path}/${props.value}`)}}>
+            <form id="navbar-search-form" onSubmit={(e) => {e.preventDefault(); if (props.value !== '') {history.push(`/${props.path}/${props.value}`)}}}>
                 <Icon icon={(props.icon != null) ? props.icon : 'search'}/>
                 <input type='text' placeholder={props.label} value={props.value} className={(props.value && 'has-text')} onChange={(e) => props.raiseStateInput(e.target.value)} onFocus={() => setFocused(true)} onBlur={() => setFocused(false)}></input>
                 {(focused || props.value) && <button className="reset-search" type="reset" onClick={() => props.raiseStateInput("")}>X</button>}
