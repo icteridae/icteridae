fb4765ba103d5c50255f9ac8c9e98c6d5f30f60d
Inluminous <lenmisch@gmail.com>
2021-03-15 21:27:55 +0100

    Add explanation screen on inactive localstorage

--- frontend/src/Components/SavedPapers/PageSavedPapersLocalstorage.tsx
+++ frontend/src/Components/SavedPapers/PageSavedPapersLocalstorage.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Sorry } from '../General/Sorry';
 import { PageSavedPapers } from './PageSavedPapers';
 
 export const SavedPapers: React.FC = () => (
-    localStorage ? <PageSavedPapers/> : <p style={{textAlign: 'center'}}><br/><br/> It seems that you have disabled the use of localStorage in your browser. <br/> To use this functionality, please enable localstorage. <br/> Except for saving papers, all functions are still available.</p>
+    localStorage ? <PageSavedPapers/> : 
+        <Sorry
+            message="Your localstorage seems inaccessible"
+            description="To use this functionality, please enable localstorage. Except for saving papers, all functions are still available."
+        />
 )
